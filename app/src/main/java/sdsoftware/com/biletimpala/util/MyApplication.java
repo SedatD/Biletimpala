f1a57fd7e71df4abe8a5ebae78d8a28f248d6bef
SedatD <dilmacsedat@gmail.com>
2018-11-18 00:16:16 +0300

    init setup

---
+++ app/src/main/java/sdsoftware/com/biletimpala/util/MyApplication.java
@@ -0,0 +1,53 @@
+package sdsoftware.com.biletimpala.util;
+
+import android.app.Application;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by SD
+ * on 25.07.2018.
+ */
+
+public class MyApplication extends Application {
+
+    private static MyApplication _instance;
+    private RequestQueue _requestQueue;
+    private SharedPreferences _preferences;
+
+    public static MyApplication get() {
+        return _instance;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        /*FirebaseAnalytics.getInstance(this);
+
+        OneSignal.startInit(this)
+                .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
+                .unsubscribeWhenNotificationsAreDisabled(true)
+                .init();*/
+
+        _instance = this;
+        _preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        _requestQueue = Volley.newRequestQueue(this);
+    }
+
+    public RequestQueue getRequestQueue() {
+        return _requestQueue;
+    }
+
+    public SharedPreferences getPreferences() {
+        return _preferences;
+    }
+
+    public SharedPreferences.Editor getPreferencesEditor() {
+        return _preferences.edit();
+    }
+
+}
